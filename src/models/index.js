d4347ebbf1b8fecbab1c6226ac53dcf50fe1e431
ravi <sumanrajak2602@gmail.com>
2023-12-16 09:18:28 +0530

    Death Status NodeJS server

---
+++ src/models/index.js
@@ -0,0 +1,24 @@
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
+
+let messages = {
+  "0792864b48e9acbc4b1bd2738f29639f71b851ee": {
+    status: true,
+  },
+  "0x784eDE59b868882a1668d323a4e2c13C0DAE9E39": {
+    status: true,
+  },
+};
+
+export default {
+  users,
+  messages,
+};
